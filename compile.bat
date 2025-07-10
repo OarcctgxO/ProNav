65e00e7417bea1c03328c2f292f7b99366b79f19
Aleksandr Tok <alexandrtok24@gmail.com>
2025-07-10 14:48:01 +0300

    Обновил "компилятор" для arcade

--- compile.bat
+++ compile.bat
@@ -1,2 +1,14 @@
-color 2
-python -m PyInstaller --onefile --add-data "land.png;." --noconsole main.py
+python -m PyInstaller --onefile ^
+    --add-data "land.png;." ^
+    --add-data "aircraft.png;." ^
+    --add-data "missile.png;." ^
+    --noconsole ^
+    --hidden-import arcade ^
+    --hidden-import arcade.gl ^
+    --hidden-import arcade.gl.provider ^
+    --hidden-import arcade.gl.backends ^
+    --hidden-import arcade.gl.backends.opengl ^
+    --hidden-import arcade.gl.backends.opengl.provider ^
+    --hidden-import pyglet ^
+    --hidden-import pyglet.gl ^
+    main.py
